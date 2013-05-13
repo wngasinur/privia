c2d8179d256dbb749619bc1a5eca5ba71a67fce7
Wiyanto Ngasinur <wngasinur@gmail.com>
2013-05-14 00:15:34 +0800

    Form validation

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,11 +2,26 @@
 /*
  * GET users listing.
  */
+ var mongoose = require('mongoose'),
+ User = mongoose.model('User')
 
-exports.list = function(req, res){
-  res.render('userList', { title: 'Express' });
-};
+ exports.list = function(req, res){
+ 	res.render('userList', { title: 'Express' });
+ };
 
-exports.add = function(req, res){
-  res.render('userForm', { title: 'Express' });
-};
+ exports.add = function(req, res){
+ 	if(req.method=='POST') {
+ 		console.log(req.body);
+ 		var user = new User(req.body);
+ 		user.save(function (err) {
+		  if (err) {
+		  	console.log(err)
+		  	return res.json(500, err);
+		  }// saved!
+		  res.json( { success: 'true' });
+
+		})	
+ 	}
+ 	else 
+ 		res.render('userForm', { success: 'Express' });
+ };
